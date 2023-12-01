fd2dd600dbea32f618c5136fef59cd05356ab1d8
Kaio Silveira <silveira.kaio@icloud.com>
2023-12-01 11:50:10 +0000

    day-01: add main execution challenge

---
+++ day-01/lib/main.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/solution'
+
+input = File.readlines(File.join(__dir__, "../data/input.txt"))
+
+part_one_result = AoC2023::Day01::Part01.get_combined_calibration_value(input: input)
+part_two_result = AoC2023::Day01::Part02.get_combined_calibration_value(input: input)
+
+puts "Part 01: #{part_one_result}" # Part 01: 55029
+puts "Part 02: #{part_two_result}"
