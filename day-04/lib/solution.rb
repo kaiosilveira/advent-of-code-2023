fc470aa72346064391337228a53bd62fc4f7c425
Kaio Silveira <silveira.kaio@icloud.com>
2023-12-04 10:21:31 +0000

    day-04: add solution for part-02

--- day-04/lib/solution.rb
+++ day-04/lib/solution.rb
@@ -1,5 +1,16 @@
 module AoC2023
   module Day04
+    def self.parse_card_information(line)
+      card_name, card_data = line.split(":")
+      winning_numbers, owned_numbers = card_data.split("|").map(&:strip)
+
+      {
+        id: card_name.split(" ").last.to_i,
+        winning_numbers: winning_numbers.split.map(&:to_i),
+        owned_numbers: owned_numbers.split.map(&:to_i)
+      }
+    end
+
     def self.find_owned_winning_numbers(card_info)
       card_info[:winning_numbers] & card_info[:owned_numbers]
     end
@@ -19,8 +30,44 @@ module AoC2023
     end
 
     module Part02
-      def self.solve(input:)
-        1
+      def self.process_turn(card_played:, all_cards:)
+        owned_winning_numbers = Day04.find_owned_winning_numbers(card_played)
+
+        card_ids_to_select = 1.upto(owned_winning_numbers.size).map do |n|
+          card_played[:id] + n
+        end
+
+        newly_acquired_card_copies = all_cards.clone.select do |card|
+          card_ids_to_select.include?(card[:id])
+        end
+
+        { newly_acquired_card_copies: newly_acquired_card_copies }
+      end
+
+      def self.get_final_card_count(initial_hand:)
+        mappings = {}
+        processed_queue = {}
+        initial_hand.each do |card|
+          turn_result = process_turn(card_played: card, all_cards: initial_hand)
+          mappings[card[:id]] = turn_result[:newly_acquired_card_copies].map { |c| c[:id] }
+          processed_queue[card[:id]] = 0
+        end
+
+        queue = mappings.keys
+        processed_item_count = 0
+        while queue.any?
+          processed_item_count += 1
+          card_id = queue.shift
+          new_cards = mappings[card_id]
+          new_cards.each { |new_card_id| queue << new_card_id }
+        end
+
+        processed_item_count
+      end
+
+      def self.solve(scratchcards:)
+        parsed_cards = scratchcards.map { |line| Day04.parse_card_information(line) }
+        get_final_card_count(initial_hand: parsed_cards)
       end
     end
   end
--- day-04/test/solution.spec.rb
+++ day-04/test/solution.spec.rb
@@ -53,8 +53,56 @@ module AoC2023
 
     module Part02
       class Part02Test < Test::Unit::TestCase
-        def test_works
-          assert_equal 1, Part02.solve(input: "1abc2")
+        def test_process_turn
+          card_01, card_02, card_03, card_04, card_05 = all_cards
+          newly_acquired_card_copies = [card_02, card_03, card_04, card_05]
+          result = Part02.process_turn(card_played: card_01, all_cards: all_cards)
+
+          assert_equal newly_acquired_card_copies, result[:newly_acquired_card_copies]
+        end
+
+        def test_get_final_card_count
+          assert_equal 30, Part02.get_final_card_count(initial_hand: all_cards)
+        end
+
+        def all_cards
+          card_01 = {
+            id: 1,
+            winning_numbers: [41, 48, 83, 86, 17],
+            owned_numbers: [83, 86, 6, 31, 17, 9, 48, 53]
+          }
+
+          card_02 = {
+            id: 2,
+            winning_numbers: [13, 32, 20, 16, 61],
+            owned_numbers: [61, 30, 68, 82, 17, 32, 24, 19]
+          }
+
+          card_03 = {
+            id: 3,
+            winning_numbers: [1, 21, 53, 59, 44],
+            owned_numbers: [69, 82, 63, 72, 16, 21, 14, 1]
+          }
+
+          card_04 = {
+            id: 4,
+            winning_numbers: [41, 92, 73, 84, 69],
+            owned_numbers: [59, 84, 76, 51, 58, 5, 54, 83]
+          }
+
+          card_05 = {
+            id: 5,
+            winning_numbers: [87, 83, 26, 28, 32],
+            owned_numbers: [88, 30, 70, 12, 93, 22, 82, 36]
+          }
+
+          card_06 = { 
+            id: 6,
+            winning_numbers: [31, 18, 13, 56, 72],
+            owned_numbers: [74, 77, 10, 23, 35, 67, 36, 11]
+          }
+
+          [card_01, card_02, card_03, card_04, card_05, card_06]
         end
       end
     end
