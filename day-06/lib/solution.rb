57794cd6c0474d72648079bdb4fad6d61689dcf4
Kaio Silveira <silveira.kaio@icloud.com>
2023-12-06 07:39:24 +0000

    day-06: add solution

---
+++ day-06/lib/solution.rb
@@ -0,0 +1,80 @@
+module AoC2023
+  module DayN
+    module Part01
+      def self.solve(input:)
+        get_product_of_record_breaking_possibilities(
+          records: parse_input(boat_race_records: input)
+        )
+      end
+
+      def self.get_product_of_record_breaking_possibilities(records:)
+        records.map do |record|
+          distributions = get_possible_acceleration_distributions_for(record: record)
+          record_breaking_distributions = get_record_breaking_acceleration_distributions(
+            target_distance: record[:distance],
+            distributions: distributions
+          )
+          record_breaking_distributions.size
+        end.reduce(&:*)
+      end
+
+      def self.get_record_breaking_acceleration_distributions(target_distance:, distributions:)
+        distributions.select { |distribution| distribution[:total_distance] > target_distance }
+      end
+
+      def self.get_possible_acceleration_distributions_for(record:)
+        (1..record[:time] - 1).map.with_index do |acceleration, idx|
+          available_time = record[:time] - acceleration
+          { acceleration: acceleration, total_distance: available_time * acceleration }
+        end
+      end
+
+      def self.parse_input(boat_race_records:)
+        records = boat_race_records
+          .map { |record| record.split(":")[1] }
+          .map { |record| record.split(" ").map(&:to_i) }
+          .transpose
+          .map { |record| { time: record[0], distance: record[1] } }
+
+          records
+      end
+    end
+
+    module Part02
+      def self.solve(input:)
+        record = parse_input(input: input)
+        get_number_of_record_breaking_acceleration_distributions_for(record: record)
+      end
+
+      def self.parse_input(input:)
+        time, distance = input.map { |record| record.split(":")[1].split(' ').join.to_i }
+        { time: time, distance: distance }
+      end
+
+      def self.get_number_of_record_breaking_acceleration_distributions_for(record:)
+        time_range = (1..record[:time] - 1)
+
+        record_breaking_range = [
+          get_first_acceleration_that_breaks_record(time_range: time_range, record: record),
+          get_first_acceleration_that_breaks_record(
+            time_range: time_range.last.downto(time_range.first), record: record
+          )
+        ]
+
+        range_start = time_range.begin + record_breaking_range.first
+        range_end = time_range.end - record_breaking_range.last
+
+        range_end - range_start + 1
+      end
+
+      def self.get_first_acceleration_that_breaks_record(time_range:, record:)
+        time_range.each.with_index do |acceleration, idx|
+          available_time = record[:time] - acceleration
+          total_distance = available_time * acceleration
+          breaks_record = total_distance > record[:distance]
+          return idx if breaks_record
+        end
+      end
+    end
+  end
+end
---
+++ day-06/test/solution.spec.rb
@@ -0,0 +1,95 @@
+require 'test/unit'
+require_relative '../lib/solution'
+
+module AoC2023
+  module DayN
+    module Part01
+      class Part01Test < Test::Unit::TestCase
+        def test_parse_input
+          records = [
+            { time: 7, distance: 9 },
+            { time: 15, distance: 40 },
+            { time: 30, distance: 200 },
+          ]
+
+          assert_equal records, Part01.parse_input(boat_race_records: input)
+        end
+
+        def test_get_possible_acceleration_distributions
+          record = { time: 7, distance: 9 }
+
+          assert_equal [
+            { acceleration: 1, total_distance: 6 },
+            { acceleration: 2, total_distance: 10 },
+            { acceleration: 3, total_distance: 12 },
+            { acceleration: 4, total_distance: 12 },
+            { acceleration: 5, total_distance: 10 },
+            { acceleration: 6, total_distance: 6 },
+          ], Part01.get_possible_acceleration_distributions_for(record: record)
+        end
+
+        def test_get_record_breaking_acceleration_distributions
+          target_distance = 9
+          distributions = [
+            { acceleration: 1, total_distance: 6 },
+            { acceleration: 2, total_distance: 10 },
+            { acceleration: 3, total_distance: 12 },
+            { acceleration: 4, total_distance: 12 },
+            { acceleration: 5, total_distance: 10 },
+            { acceleration: 6, total_distance: 6 },
+          ]
+
+          assert_equal [
+            { acceleration: 2, total_distance: 10 },
+            { acceleration: 3, total_distance: 12 },
+            { acceleration: 4, total_distance: 12 },
+            { acceleration: 5, total_distance: 10 },
+          ], Part01.get_record_breaking_acceleration_distributions(
+            target_distance: target_distance,
+            distributions: distributions
+          )
+        end
+
+        def test_get_product_of_record_breaking_possibilities
+          records = [
+            { time: 7, distance: 9 },
+            { time: 15, distance: 40 },
+            { time: 30, distance: 200 },
+          ]
+
+          assert_equal 288, Part01.get_product_of_record_breaking_possibilities(records: records)
+        end
+
+        def input
+          [
+            "Time:      7  15   30",
+            "Distance:  9  40  200"
+          ]
+        end
+      end
+    end
+
+    module Part02
+      class Part02Test < Test::Unit::TestCase
+        def test_parse_input
+          expected_result = { time: 71530, distance:  940200 }
+          assert_equal expected_result, Part02.parse_input(input: input)
+        end
+
+        def test_get_record_breaking_acceleration_distributions
+          assert_equal 4, Part02.get_number_of_record_breaking_acceleration_distributions_for(record: { time: 7, distance: 9 })
+          assert_equal 8, Part02.get_number_of_record_breaking_acceleration_distributions_for(record: { time: 15, distance: 40 })
+          assert_equal 9, Part02.get_number_of_record_breaking_acceleration_distributions_for(record: { time: 30, distance: 200 })
+          assert_equal 71503, Part02.get_number_of_record_breaking_acceleration_distributions_for(record: { time: 71530, distance: 940200 })
+        end
+
+        def input
+          [
+            "Time:      7  15   30",
+            "Distance:  9  40  200"
+          ]
+        end
+      end
+    end
+  end
+end
